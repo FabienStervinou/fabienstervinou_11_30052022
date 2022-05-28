4adac0e54d685e2f302496536933892cfeae9693
Fabien Stervinou <stervinoufabien@gmail.com>
2022-05-28 10:46:07 +0200

    Create <Error404> view

---
+++ src/views/Error404/index.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+import { Link } from 'react-router-dom'
+
+export function Error404 () {
+
+  return (
+  <main className='error'>
+    <h1 className='error-title'>404</h1>
+    <div className="error-description">
+      Oups ! La page que vous demandez n'existe pas
+    </div>
+    <Link className="error-link" to="/">Retourner sur la page d'accueil</Link>
+  </main>
+  )
+}
