0ab2f9d6a624a9a51226c35a674432bf672037da
Fabien Stervinou <stervinoufabien@gmail.com>
2022-05-28 10:48:03 +0200

    Create <Header> layout

---
+++ src/layout/Header/index.js
@@ -0,0 +1,8 @@
+import { NavBar } from "../Navigation/NavBar/index";
+import './style.scss'
+
+export function Header () {
+  return <header className='header'>
+    <NavBar></NavBar>
+  </header>
+}
