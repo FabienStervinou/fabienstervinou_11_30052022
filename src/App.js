28b942c2e6415500c64532568c05021284d69021
Fabien Stervinou <stervinoufabien@gmail.com>
2022-05-28 10:47:35 +0200

    Add Navigation logic on <App>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import './main.scss';
+import { Header } from './layout/Header/index'
+import { Home } from './views/home/index'
+import { About } from './views/about/index'
+import { Error404 } from './views/Error404/index'
+import { Property } from './views/Property/index.js'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
-function App() {
+export function App () {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route exact path="/" element={<Home/>} /> 
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/property/:propertyId" element={<Property/>} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
