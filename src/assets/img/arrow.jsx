9ebbd950e3510b9989ba2d1ea12e0eda9b392534
Fabien Stervinou <stervinoufabien@gmail.com>
2022-05-30 23:56:07 +0200

    import svg

---
+++ src/assets/img/arrow.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function MaterialSymbolsArrowBackIosNew(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M16 22L6 12L16 2l1.775 1.775L9.55 12l8.225 8.225Z"></path></svg>
+  )
+}
+export default MaterialSymbolsArrowBackIosNew
---
+++ src/assets/img/starIMG.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Star(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m5.825 22l2.325-7.6L2 10h7.6L12 2l2.4 8H22l-6.15 4.4l2.325 7.6L12 17.3Z"></path></svg>
+  )
+}
+export default Star
