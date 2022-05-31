50e0f4d2019e9b5e8b2aca21b21b3ecf462e4f8e
Fabien Stervinou <stervinoufabien@gmail.com>
2022-05-31 08:40:20 +0200

    Update <Header>

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -2,7 +2,9 @@ import { NavBar } from "../Navigation/NavBar/index";
 import './style.scss'
 
 export function Header () {
-  return <header className='header'>
-    <NavBar></NavBar>
-  </header>
+  return (
+    <header className='header'>
+      <NavBar />
+    </header>
+  )
 }
